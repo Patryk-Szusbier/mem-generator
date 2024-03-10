bab70c88c6f4386273a8b98bf54647155a5152a6
Patryk Szusbier <93617702+Patryk-Szusbier@users.noreply.github.com>
2024-03-10 19:50:22 +0100

    Wyświetlanie losowego obrazu na ekranie

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,17 +2,14 @@ import React, {useState} from "react"
 import memesData from "../memesData.js"
 
 export default function Meme() {
-    let url;
-    const [num, setNum] = useState(0);
-
-    const randomNumberInRange = (min, max) => {
-        return Math.floor(Math.random()
-            * (max - min + 1)) + min;
-    };
-    const handleClick = () => {
-        setNum(randomNumberInRange(1, 100));
-        url = memesData.data.memes[num].url
-    };
+    
+    const [memeURL,getMemeURL] = useState("")
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        getMemeURL(memesArray[randomNumber].url)  
+        
+    }
     return(
         <main>
             <div className="meme--form">
@@ -41,7 +38,7 @@ export default function Meme() {
                 className="meme--button" onClick={handleClick}>
                     Get a new meme image 🖼
                 </button>
-                <p>{url}</p>
+                <img src={memeURL} className="meme--Image"/>
             </div>
         </main>
     );
